73e62d6cdfbd63493f4879654b18e58afc1022d2
bacloud14 <bacloud14@gmail.com>
2021-01-04 10:45:40 +0100

    pan location & added menu and footer in other pages

--- GMap.js
+++ GMap.js
@@ -21,6 +21,7 @@ function initMap() {
             lng: coordinates[0]
         }
     }
+
     if (!map) {
         map = new google.maps.Map(document.getElementById("map"), {
             center: center || {
@@ -148,10 +149,54 @@ function initMap() {
         getPicture(place.name);
         nearbyRequest(place);
     });
+
+    var panButton = document.getElementsByClassName("custom-map-control-button")[0];
+    if(panButton)
+        return;
+    infoWindow = new google.maps.InfoWindow();
+    const locationButton = document.createElement("button");
+    locationButton.textContent = "Go to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(locationButton);
+    locationButton.addEventListener("click", () => {
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("Location found.");
+            infoWindow.open(map);
+            map.setCenter(pos);
+          },
+          () => {
+            handleLocationError(true, infoWindow, map.getCenter());
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    });
+  
     // Populate current list of cities on a floating HTML panel on the map
     // showplacesList(currentList);
 
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+  }
+  
 
 // Create an AJAX request for one place this is called once the user search for a city. "nearby/" is the main API in back-end
 function nearbyRequest(place) {
